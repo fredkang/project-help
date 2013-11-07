a2aaae1eabfe7d9d948906321b4abfc84a1d8cf2
Fred Kang <fredkang@gmail.com>
2013-11-06 16:46:04 -0800

    Edited the message notification email footer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,7 +12,7 @@ class UserMailer < ActionMailer::Base
   	@user = user
   	@message = message
   	@sender = sender
-  	@url = 'http://localhost:3000/inbox/' + convoID.to_s
+  	@url = 'http://www.projecthelp.co/inbox/' + convoID.to_s
 
   	mail :to		=> user.email,
   		 :subject	=> "New message from " + @sender.first_name
